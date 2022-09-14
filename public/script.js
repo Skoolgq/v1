198d43819830699ca6d591b2d28c6ceabc123427
fowntain <93336301+fowntain@users.noreply.github.com>
2022-09-14 14:03:48 -0400

    Delete public directory

--- public/script.js
+++
@@ -1,5 +0,0 @@
-if(document.location.href.indexOf('school') > -1) { 
-        // indexOf will return the position of the first occurence of this string in the url
-       // or -1 it it's not there.
-       document.location.href = 'http://skool.gq/newurl.html';
-      }
