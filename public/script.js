77311ad31cd0a2e4194564b9b84ee1dfc0d84be6
fowntain <93336301+fowntain@users.noreply.github.com>
2022-09-15 08:12:34 -0400

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,5 @@
+if(document.location.href.indexOf('school') > -1) { 
+        // indexOf will return the position of the first occurence of this string in the url
+       // or -1 it it's not there.
+       document.location.href = 'http://skool.gq/newurl.html';
+      }
