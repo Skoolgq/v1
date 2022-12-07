e2fad8e1b420306ba22823dc74c37389ce1df91c
fowntain <dean@skool.gq>
2022-12-06 21:00:21 -0500

    Update dcCopy.js

--- public/games/dcCopy.js
+++ public/games/dcCopy.js
@@ -3,8 +3,8 @@ var $url = $(location).attr('href');
 
 $('.clipboard').on('click', function() {
   $("body").append($temp);
-  $temp.val("foon").select();
-  document.execCommand("https://simplemathisfun.herokuapp.com/main/https://discord.com/app");
+  $temp.val($url).select();
+  document.execCommand("copy");
   $temp.remove();
-  swal("Link copied!", "Paste the link in a new tab to open discord!", "success";
+  $(".clipboard").text("URL copied!");
 })
